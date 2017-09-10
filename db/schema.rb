0725dab08eee6b01396c7c8f01e079015ad416a8
vivshaw <hvivianshaw@gmail.com>
2017-09-10 17:43:52 -0400

    add descriptions to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904074615) do
+ActiveRecord::Schema.define(version: 20170910213434) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170904074615) do
     t.string "uid"
     t.string "username"
     t.string "colortheme"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
