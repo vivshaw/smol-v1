39687d1c614acfc5efcca7f05104c56e000ed6d1
vivshaw <hvivianshaw@gmail.com>
2017-09-11 00:47:42 -0400

    add top posts to landing page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :faves, dependent: :destroy
   has_many :favers, through: :faves, source: :users
+
+  def self.top3
+    return Post.left_joins(:faves)
+        .group(:id)
+        .order('COUNT(faves.id) DESC')
+        .limit(3)
+  end
+
 end
