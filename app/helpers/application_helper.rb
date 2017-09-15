8781fff276440f966a9d260b2dc58185ce977825
vivshaw <hvivianshaw@gmail.com>
2017-09-15 00:29:24 -0400

    render post fulltext with markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def markdown(text)
+    markdowner = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true)
+
+    return markdowner.render(text).html_safe
+  end
+
 end
